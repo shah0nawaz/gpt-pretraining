adddd308892b82b2acf4289741fea37bf0842e63
shah0nawaz <shahnawaz.mct59@gmail.com>
2025-08-21 11:20:54 +0500

    mm

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def text_to_ids(text, tokenizer):
     ids = torch.tensor(tokenizer.encode(text))
@@ -74,3 +75,76 @@ def generate(model, idx, max_new_tokens,
         idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
 
     return idx
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+
+def load_weights_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+    for b in range(len(params["blocks"])):
+        q_w, k_w, v_w  = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+    
+        gpt.trf_blocks[b].mha.W_Q.weight  = assign(
+            gpt.trf_blocks[b].mha.W_Q.weight, q_w.T
+        )
+        gpt.trf_blocks[b].mha.W_K.weight  = assign(
+            gpt.trf_blocks[b].mha.W_K.weight, k_w.T
+        )
+        gpt.trf_blocks[b].mha.W_V.weight  = assign(
+            gpt.trf_blocks[b].mha.W_V.weight, v_w.T
+        )
+    
+        q_b, k_b, v_b = np.split(
+        (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].mha.W_Q.bias = assign(
+            gpt.trf_blocks[b].mha.W_Q.bias, q_b)
+        gpt.trf_blocks[b].mha.W_K.bias = assign(
+            gpt.trf_blocks[b].mha.W_K.bias, k_b)
+        gpt.trf_blocks[b].mha.W_V.bias = assign(
+            gpt.trf_blocks[b].mha.W_V.bias, v_b)
+    
+    
+        gpt.trf_blocks[b].mha.proj.weight = assign(
+            gpt.trf_blocks[b].mha.proj.weight,
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        
+        gpt.trf_blocks[b].mha.proj.bias = assign(
+            gpt.trf_blocks[b].mha.proj.bias,
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+        
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight,
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias,
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight,
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias,
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+        
+        gpt.trf_blocks[b].layernorm1.scale = assign(
+            gpt.trf_blocks[b].layernorm1.scale,
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].layernorm1.shift = assign(
+            gpt.trf_blocks[b].layernorm1.shift,
+             params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].layernorm2.scale = assign(
+        gpt.trf_blocks[b].layernorm2.scale,
+        params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].layernorm2.shift = assign(
+            gpt.trf_blocks[b].layernorm2.shift,
+            params["blocks"][b]["ln_2"]["b"])
+    
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
